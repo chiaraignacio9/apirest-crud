36dbe87c77586b89e40cd786b1f46c3734dd7769
chiaraignacio9 <ignaciochiara2@gmail.com>
2022-12-31 18:33:21 -0300

    Update README.md

--- src/main/java/apirestcrud/app/controllers/ProductController.java
+++ src/main/java/apirestcrud/app/controllers/ProductController.java
@@ -15,11 +15,6 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
-    @GetMapping("/index")
-    public String index(){
-        return "index";
-    }
-
     @GetMapping("/products")
     public List<Product> findAll(){
         return productService.findAll();
